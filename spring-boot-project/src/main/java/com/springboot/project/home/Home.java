41ed5189b12ab1362d8d31c20b3c8eb6e694ce59
jinhyuk <kjh78951@gmail.com>
2021-03-30 02:34:14 +0900

    Spring Boot Setting #2

---
+++ spring-boot-project/src/main/java/com/springboot/project/home/Home.java
@@ -0,0 +1,12 @@
+package com.springboot.project.home;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
