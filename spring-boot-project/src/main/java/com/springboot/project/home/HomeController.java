a7b536b35ac7f1f763891c62ab8fec3dfeeb17c9
jinhyuk <kjh78951@gmail.com>
2021-03-30 22:13:17 +0900

    SpringBoot 기본세팅

--- spring-boot-project/src/main/java/com/springboot/project/home/HomeController.java
+++ spring-boot-project/src/main/java/com/springboot/project/home/HomeController.java
@@ -1,10 +1,10 @@
 package com.springboot.project.home;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class Home {
+@Controller
+public class HomeController {
 	@RequestMapping("/home")
 	public String home() {
 		return "home";
