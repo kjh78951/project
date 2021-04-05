2741886cd44f0a73b948250075b8deccb6be7de7
jinhyuk <kjh78951@gmail.com>
2021-04-06 01:33:31 +0900

    vue.js 템플릿 적용 - 빌드

--- spring-boot-project/vue-project/vue.config.js
+++ spring-boot-project/vue-project/vue.config.js
@@ -1,6 +1,16 @@
+/**
+ * outputDir은 npm run build로 빌드 시 파일이 생성되는 위치,
+ * indexPath는 index.html 파일이 생성될 위치를 지정,
+ * devServer는 Back-End , 즉 Spring Boot의 내장 was의 주소를 작성하면 됩니다.
+ */
 const webpack = require('webpack');
 
 module.exports = {
+  outputDir: "../src/main/resources/static",
+  indexPath: "../static/index.html",  
+  devServer: {  
+    proxy: "http://localhost:8080"  
+  },
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
