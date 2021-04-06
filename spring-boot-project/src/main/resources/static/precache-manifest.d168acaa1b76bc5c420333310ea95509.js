15f47534688e2431d931d5eae1ef03f7c791bfad
jinhyuk <kjh78951@gmail.com>
2021-04-07 01:18:14 +0900

    path # 제거

--- spring-boot-project/src/main/resources/static/precache-manifest.d168acaa1b76bc5c420333310ea95509.js
+++ spring-boot-project/src/main/resources/static/precache-manifest.d168acaa1b76bc5c420333310ea95509.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "b2c444f9d747c56c3a6c",
+    "revision": "dac94fc486597df766b1",
     "url": "/css/app.3ca45e04.css"
   },
   {
-    "revision": "b2c444f9d747c56c3a6c",
-    "url": "/js/app.91f693a2.js"
+    "revision": "dac94fc486597df766b1",
+    "url": "/js/app.f687b35b.js"
   },
   {
-    "revision": "4abf18f4f2a94ffcb73d",
+    "revision": "99e728cb0b62011d47a1",
     "url": "/css/chunk-4e0b8328.1fead564.css"
   },
   {
-    "revision": "4abf18f4f2a94ffcb73d",
-    "url": "/js/chunk-4e0b8328.caa23c07.js"
+    "revision": "99e728cb0b62011d47a1",
+    "url": "/js/chunk-4e0b8328.324bcc73.js"
   },
   {
-    "revision": "ac09227a287496485542",
-    "url": "/js/chunk-52e4de86.af39a239.js"
+    "revision": "1eb7a62ae8c7f43afe52",
+    "url": "/js/chunk-52e4de86.b2a8b414.js"
   },
   {
     "revision": "3dd28c904d8329989364",
@@ -32,28 +32,28 @@ self.__precacheManifest = [
     "url": "/fonts/nucleo-icons.42643978.woff2"
   },
   {
-    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
-    "url": "/fonts/nucleo-icons.2569aaea.woff"
-  },
-  {
     "revision": "f82ec6ba2dc4181db2af35c499462840",
     "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
   },
   {
     "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
     "url": "/img/nucleo-icons.0b8a30b1.svg"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/fonts/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
@@ -64,7 +64,7 @@ self.__precacheManifest = [
     "url": "/img/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "4e661defc1eef6841e984c2745f85697",
+    "revision": "cf0cc8086317edfec590a84e0d3d7dc0",
     "url": "/index.html"
   },
   {
@@ -72,16 +72,16 @@ self.__precacheManifest = [
     "url": "/img/brand/blue.png"
   },
   {
-    "revision": "c85c75275c0a0a617f9e5accc2700908",
-    "url": "/img/brand/creativetim-white-slim.png"
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
   },
   {
     "revision": "b9949387c6179e2dc4c675134a7b7935",
     "url": "/img/brand/favicon.png"
   },
   {
-    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
-    "url": "/img/brand/github-white-slim.png"
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
   },
   {
     "revision": "6fafe4baca9d50d61a898c84ade7afa3",
@@ -96,10 +96,6 @@ self.__precacheManifest = [
     "url": "/img/ill/ill-2.svg"
   },
   {
-    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
-    "url": "/img/theme/img-2-1200x1000.jpg"
-  },
-  {
     "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
     "url": "/img/theme/profile.jpg"
   },
@@ -108,8 +104,16 @@ self.__precacheManifest = [
     "url": "/img/theme/img-1-1200x1000.jpg"
   },
   {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
     "revision": "974088a1931e40895bac6db119c62448",
     "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
   },
   {
     "revision": "edc7106b21ec12e57022b2ebd534cd2d",
@@ -122,10 +126,6 @@ self.__precacheManifest = [
   {
     "revision": "66618a418175ddf2ac8c47a241d327a8",
     "url": "/img/theme/team-4-800x800.jpg"
-  },
-  {
-    "revision": "be997d5226b992ffad34816870c6b7aa",
-    "url": "/img/theme/team-2-800x800.jpg"
   },
   {
     "revision": "aedeb218ec90680dadcd73acfdc7269e",
--- spring-boot-project/src/main/resources/static/service-worker.js
+++ spring-boot-project/src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a856c0ae9e8664ebe413e3f394d24b87.js"
+  "/precache-manifest.d168acaa1b76bc5c420333310ea95509.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-design-system"});
